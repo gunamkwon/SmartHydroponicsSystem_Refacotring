e670d774636b104d9fbf6fce2d3401f3806b0b67
gunamkwon <rnska96@mju.ac.kr>
2022-01-27 15:48:53 +0900

    feat: ControlSystemFragment 상세 xml 생성 및 fragement 생성
    1. ControSystemFragment 안에 두 가지 탭 들어갈 Fragement 생성 ( SensorStatusFragment / SensorControlFragment )
    2. 해당 XML 생성 및 sensor status item 통해 Sensor Item 표시할 XML 생성

---
+++ SmartHydroponicsSystem/app/src/main/java/com/example/smarthydroponicssystem/SensorStatusFragment.kt
@@ -0,0 +1,47 @@
+package com.example.smarthydroponicssystem
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.example.smarthydroponicssystem.databinding.FragmentControlSystemBinding
+import com.example.smarthydroponicssystem.databinding.FragmentSensorStatusBinding
+import com.example.smarthydroponicssystem.databinding.SensorStatusItemBinding
+
+class SensorStatusFragment: Fragment() {
+
+    private lateinit var binding: FragmentSensorStatusBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSensorStatusBinding.inflate(layoutInflater, container, false)
+
+
+        bindTdsSensorView()
+        bindWaterSensorView()
+
+        return binding.root
+    }
+
+
+    private fun bindTdsSensorView() {
+
+        binding.tdsSensorStatus.btnStatusDetail.setOnClickListener {
+
+        }
+    }
+
+    private fun bindWaterSensorView() {
+
+        binding.waterSensorStatus.btnStatusDetail.setOnClickListener {
+
+            //binding.waterSensorStatus.itemCardView.addView(createDetailInfo())
+        }
+    }
+
+}
