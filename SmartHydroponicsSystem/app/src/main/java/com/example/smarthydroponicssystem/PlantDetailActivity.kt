94a8c1d77d1d54c6fa2eb3d1ba74947e95a78fe0
gunamkwon <rnska96@mju.ac.kr>
2022-02-01 23:23:46 +0900

    feat: Plant Detail Activity UI 구성
    CoordinatorLayout + CollaspingAppBar  Layout을 이용하여 구성

--- SmartHydroponicsSystem/app/src/main/java/com/example/smarthydroponicssystem/PlantDetailActivity.kt
+++ SmartHydroponicsSystem/app/src/main/java/com/example/smarthydroponicssystem/PlantDetailActivity.kt
@@ -5,13 +5,21 @@ import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
+import com.example.smarthydroponicssystem.databinding.ActivityPlantDetailBinding
 import com.example.smarthydroponicssystem.model.Plant
 
 class PlantDetailActivity: AppCompatActivity() {
 
+    val binding by lazy {
+        ActivityPlantDetailBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
 
         Log.d("PlantDetailActivity", "${intent.getParcelableExtra<Plant>("item")?.name}")
+
     }
 }
