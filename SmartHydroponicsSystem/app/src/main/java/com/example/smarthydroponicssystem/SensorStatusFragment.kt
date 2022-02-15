87328cdeaf781c3ad8210be82d34381c6f6e66ad
gunamkwon <rnska96@mju.ac.kr>
2022-02-16 00:33:41 +0900

    feat: 동적으로 ConstraintLayout에 원하는 View 생성
    ConstraintSet 안에는 꼭 ID 붙어야 함
    어떤 View 들어갈지 구체화 해야함
    
    See also #2

--- SmartHydroponicsSystem/app/src/main/java/com/example/smarthydroponicssystem/SensorStatusFragment.kt
+++ SmartHydroponicsSystem/app/src/main/java/com/example/smarthydroponicssystem/SensorStatusFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
@@ -33,37 +34,32 @@ class SensorStatusFragment: Fragment() {
     private fun bindTdsSensorView() {
 
         binding.tdsSensorStatus.btnStatusDetail.setOnClickListener {
-            binding.tdsSensorStatus.layoutAddItem.addView(createDetailInfo())
+            binding.tdsSensorStatus.cardDetail.addView(createDetailInfo())
         }
+        binding.root.invalidate()
     }
 
     private fun bindWaterSensorView() {
 
         binding.waterSensorStatus.btnStatusDetail.setOnClickListener {
 
-            val set = ConstraintSet()
+           val set = ConstraintSet()
             val text = TextView(context).apply {
                 id = View.generateViewId()
                 text = "Hello World"
-                setTextColor(Color.WHITE)
+                setTextColor(Color.BLACK)
             }
-            binding.waterSensorStatus.layoutAddItem.addView(text)
-            set.clone(binding.waterSensorStatus.layoutAddItem)
+            binding.waterSensorStatus.cardDetail.addView(text)
+
+            set.clone(binding.waterSensorStatus.cardDetail)
             set.apply {
-                connect(text.id, ConstraintSet.TOP, binding.waterSensorStatus.layoutAddItem.id, ConstraintSet.TOP)
-                connect(text.id, ConstraintSet.START, binding.waterSensorStatus.layoutAddItem.id, ConstraintSet.START)
+                connect(text.id, ConstraintSet.TOP, binding.waterSensorStatus.cardDetail.id, ConstraintSet.TOP)
+                connect(text.id, ConstraintSet.START, binding.waterSensorStatus.cardDetail.id, ConstraintSet.START)
             }
         }
     }
 
     private fun createDetailInfo(): View? {
-
-        val constraintLayout = ConstraintLayout(requireContext()).apply {
-            layoutParams = ConstraintLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-        }
 
         return LayoutInflater.from(context)
             .inflate(R.layout.support_simple_spinner_dropdown_item, binding.root, false)
